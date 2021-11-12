0d69684a866ebf599e0aaf7b13863635983e24f2
Pathaksa Tongpitak <pathaksa@affiliateweapons.com>
2021-11-12 18:45:23 +0700

    update symbolic_links.sh script

--- symbolic_links.sh
+++ symbolic_links.sh
@@ -1,9 +1,18 @@
-#!/bin/bash
+#!/bin/env zsh
 
-apps=( nano )
+apps=( nano zsh )
 
-
-for i in apps; do
-    [[ -f ]]
-    ln -s ~/dotfiles/nano/.nanorc ~/.nanorc
+for i in $apps; do
+    rcfile=".$i"rc
+    source=~/dotfiles/$i/$rcfile
+    target=~/$rcfile
+    [[ -f $target ]] && {
+      echo "skipping: $i"
+      echo "$target alredy exist"
+    } || {
+      echo "Creating symbolic for: $i"
+      echo "Source: $source"
+      echo "Target: $target"
+      ln -s $source $target
+    }
 done
