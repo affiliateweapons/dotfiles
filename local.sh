4e2258ca2fb462d5c578e811dc6facb4e3fc21a5
Pathaksa Tongpitak <pathaksa@affiliateweapons.com>
2021-11-15 06:25:24 +0700

    add script to be run on local machines only

--- local.sh
+++ local.sh
@@ -1,2 +1,6 @@
-ln -s ~/dotfiles/vscode/setting.json ~/.config/VSCodium/User/settings.json
+# script should only be run on your local work machine
+
+
+# create symbolic links for vs codium settinggs and keybindings
+ln -s ~/dotfiles/vscode/settings.json ~/.config/VSCodium/User/settings.json
 ln -s ~/dotfiles/vscode/keybindings.json ~/.config/VSCodium/User/keybindings.json
